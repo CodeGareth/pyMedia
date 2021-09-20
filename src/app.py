fce90c816454a4007354ef17aac2d81186785ca9
CodeGareth <63073615+CodeGareth@users.noreply.github.com>
2021-09-20 15:59:46 +0100

    Currently utilising the youtube iframe api to play the first video from the search results // Intend to rework at this stage to actually present the search results in a grid and have the user navigate to the external youtube page

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template
 import requests
-import json
 import ast
 import random
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -15,7 +17,13 @@ def landing_page():
 @app.route("/YouTube/<query>", methods = ['GET'])
 def youtube_route(query):
 
-    return jsonify({"search_term":query})
+    youtube_search = requests.get(f"https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q={query}&key={os.getenv('YOUTUBE_API')}")
+    youtube_search_json_results = youtube_search.json()
+
+    video_id = youtube_search_json_results["items"][0]["id"]["videoId"]
+    print (video_id)
+
+    return render_template("youtube_return.html", video_id = video_id)
 
 ## Ensure the end point matches the text casing in the js 
 @app.route("/Flickr/<query>/<number_request>", methods = ['GET'])
@@ -27,15 +35,16 @@ def flickr_route(query, number_request):
 
     ## Note - Using literal eval vs eval as safer 
     ## ref: https://docs.python.org/3/library/ast.html - search or ast.literal_eval 
-    ## "Safely evaluate an expression node or a string containing a Python literal or container display. 
-    ## The string or node provided may only consist of the following Python literal structures: strings, bytes, numbers, tuples, lists, dicts, sets, booleans, and None."
     converted_flickr_response = ast.literal_eval(flickr_response.content.decode('utf-8').replace("jsonFlickrFeed",""))
 
-    random_selection_of_photos = random.choices(converted_flickr_response["items"], k = 3)
-    breakpoint()
+    ## Make a random selection from the list of available items
+    random_selection_of_photos = random.choices(converted_flickr_response["items"], k = int(number_request))
 
+    ## Convert this to html text with the intention of returning back to flask template iframe
+    media = " ".join([f"<img src = {selection['media']['m']}></img>" for selection in random_selection_of_photos])
 
-    return jsonify({"search_term":query, "total_photos":number_request, "results":random_selection_of_photos})
+    #return jsonify({"search_term":query, "total_photos":number_request, "results":random_selection_of_photos})
+    return render_template("flickr_return.html", media=media)
 
 if __name__ == "__main__":
 
