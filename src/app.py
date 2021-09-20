e1204d42e5df3f80bf2a49515443e17ea55d7a44
CodeGareth <63073615+CodeGareth@users.noreply.github.com>
2021-09-20 17:37:51 +0100

    happy path - working youtube or flickr search tool utilising two different apis

--- src/app.py
+++ src/app.py
@@ -17,13 +17,29 @@ def landing_page():
 @app.route("/YouTube/<query>", methods = ['GET'])
 def youtube_route(query):
 
-    youtube_search = requests.get(f"https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q={query}&key={os.getenv('YOUTUBE_API')}")
+    youtube_search = requests.get(f"https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=21&q={query}&key={os.getenv('YOUTUBE_API')}")
     youtube_search_json_results = youtube_search.json()
 
-    video_id = youtube_search_json_results["items"][0]["id"]["videoId"]
-    print (video_id)
+    storage_collection = []
 
-    return render_template("youtube_return.html", video_id = video_id)
+    ## Youtube Results return several types of links 
+    ## For ease - filter only to videos and the elements we need to create our own browser screen
+    for item in youtube_search_json_results["items"]:
+        
+        trimmed_item = {}
+        
+        if "video" not in item["id"]["kind"]:
+            pass
+        else:
+            trimmed_item["id"] = item["id"]["videoId"]
+            trimmed_item["description"] = item["snippet"]["description"]
+            trimmed_item["title"] = item["snippet"]["title"]
+            trimmed_item["medium_thumb"] = item["snippet"]["thumbnails"]["medium"]["url"]
+    
+            storage_collection.append(trimmed_item)
+
+    ## FYI: For loop within template converts list of dicts to html
+    return render_template("youtube_search_return.html", youtube_search_results = storage_collection)
 
 ## Ensure the end point matches the text casing in the js 
 @app.route("/Flickr/<query>/<number_request>", methods = ['GET'])
@@ -40,7 +56,7 @@ def flickr_route(query, number_request):
     ## Make a random selection from the list of available items
     random_selection_of_photos = random.choices(converted_flickr_response["items"], k = int(number_request))
 
-    ## Convert this to html text with the intention of returning back to flask template iframe
+    ## Convert this to html text with the intention of returning back to flask template iframe as is
     media = " ".join([f"<img src = {selection['media']['m']}></img>" for selection in random_selection_of_photos])
 
     #return jsonify({"search_term":query, "total_photos":number_request, "results":random_selection_of_photos})
